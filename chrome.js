12ca2ddc3925647f1396c03391cb815d28d6a829
Tchinonso <lightersmercury@gmail.com>
2022-11-11 16:07:07 +0100

    chrome extension done completely

--- chrome.js
+++ chrome.js
@@ -2,15 +2,74 @@ let myLead = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 let ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 // function btnClicked() {
 // 	console.log("button clicked")
 // }
 
+const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLead"))
+console.log(leadFromLocalStorage)
+
+if (leadFromLocalStorage) {
+	myLead = leadFromLocalStorage
+	render(myLead)
+}
 inputBtn.addEventListener('click', function () {
 	myLead.push(inputEl.value)
-	console.log(myLead)
+	localStorage.setItem("myLead", JSON.stringify(myLead))
+	inputEl.value = ""
+	render(myLead)
 })
-for (let i = 0; i < myLead.length; i++){
-	console.log(myLead[i])
+
+
+function render(lead) {
+	let listItems = ""; //third method first create an empty strings
+  for (let i = 0; i < lead.length; i++) {
+    //first method
+    //ulEl.textContent += myLead[i] + " "
+    //ulEl.innerHTML += "<li>" + myLead[i] + "</li>"
+
+    //second method
+    //create an element
+    //set text content
+    //append to ul
+    //  const li = document.createElement('li')
+    //  li.textContent = myLead[i]
+    //  ulEl.append(li)
+
+    //third method
+    //listItems += "<li><a target='_blank' href='" + myLead[i] + "'>" + myLead[i] + "</a></li>"
+	  listItems += `
+	  					<li>
+	  						<a target='_blank' href='${lead[i]}'> 
+									${lead[i]}
+							</a>
+						</li>
+					`
+  }
+  ulEl.innerHTML = listItems;
 }
+deleteBtn.addEventListener("dblclick", function () {
+	localStorage.clear()
+	myLead = []
+	render(myLead)
+})
+
+// const tabs = [
+// 	{ url: "https://www.linkedin.com/in/Tchinonso/" },
+// 	{url: "https://www.github.com/Tchinonso/"}
+// ]
+
+tabBtn.addEventListener("click", function () {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // console.log(tabs[0].url)
+    // console.log(tabs[1].url)
+    myLead.push(tabs[0].url);
+    //myLead.push(tabs[1].url);
+    localStorage.setItem("myLead", JSON.stringify(myLead));
+    render(myLead);
+  })
+	
+})
--- practice.js
+++ practice.js
@@ -3,3 +3,42 @@ let box = document.getElementById("box")
 box.addEventListener('click', function () {
 	console.log("i want to open the box")
 })
+let container = document.getElementById("container")
+container.innerHTML += "<button onclick='buy()'>Buy</button>"
+
+function buy() {
+	container.innerHTML += "<p>thank you for buying</p>"
+}
+
+//template strings/literals
+const recipient = "James"
+//create a new v sender and set it to my name
+const sender = `Tchinonso don`
+//use your sender v instead of peers
+//const email = "hey " + recipient + "! how is it going? cheers peers"
+//break the email strings into multiple lines
+const email = `hey ${recipient}! how is it going? cheers ${sender} //peers`
+console.log(email)
+
+//myLead is a string now
+let myLead = `["www.lead1.com"]`
+console.log(myLead)
+//turned the string back to an array
+myLead = JSON.parse(myLead)
+console.log(myLead)
+//turn the array into a string again
+myLead = JSON.stringify(myLead)
+console.log(myLead)
+//check the type of data myLead
+console.log(typeof myLead)
+
+// localStorage.clear()
+// localStorage.setItem()
+// localStorage.getItem()
+
+const hello = document.getElementById("hello")
+
+function user(greeting, name) {
+	hello.textContent = `${greeting}, ${name}🙌`
+}
+user('hello','chief')
